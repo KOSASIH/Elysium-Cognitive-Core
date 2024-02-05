1169edf45fffd3c94c47290353d1ae968f49e6ec
KOSASIH <kosasihg88@gmail.com>
2024-02-05 13:24:54 +0700

    Create sort_strings.py

---
+++ src/sort_strings.py
@@ -0,0 +1,12 @@
+def sort_strings(strings):
+    """
+    Sorts a list of strings in alphabetical order.
+
+    Args:
+        strings (list): A list of strings to be sorted.
+
+    Returns:
+        list: The sorted list of strings.
+    """
+    sorted_strings = sorted(strings)
+    return sorted_strings
