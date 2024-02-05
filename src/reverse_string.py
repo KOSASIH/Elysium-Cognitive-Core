62c5d0e89a7c8f925b195e98d41d07c4502309a0
KOSASIH <kosasihg88@gmail.com>
2024-02-05 13:47:57 +0700

    Create reverse_string.py

---
+++ src/reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_string(input_string):
+    """
+    Reverses the given string.
+
+    Args:
+        input_string (str): The string to be reversed.
+
+    Returns:
+        str: The reversed string.
+    """
+    return input_string[::-1]
